f3166a20289b6583f6b0ead9d95dc8bc96cf36e0
Joshua Snider <jsnider@mtu.edu>
2021-09-19 00:04:29 -0400

    Add autoscaler.tf

---
+++ autoscaler.tf
@@ -0,0 +1,60 @@
+module "as_label" {
+  source  = "cloudposse/label/null"
+  version = "0.25.0"
+  context = module.label.context
+
+  name = "autoscaler-role"
+}
+
+resource "aws_iam_role_policy" "autoscaler" {
+  name = "autoscaler-policy"
+  role = aws_iam_role.autoscaler.id
+
+  policy =<<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:DescribeTags",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup",
+                "ec2:DescribeLaunchTemplateVersions"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_role" "autoscaler" {
+  name = "autoscaler-role"
+
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "0",
+            "Effect": "Allow",
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Principal": {
+                "Federated": "arn:aws:iam::556005419303:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/188020E1CD2C0191ECE6A6C32DC95755"
+            },
+            "Condition": {
+                "StringEquals": {
+                    "oidc.eks.us-east-1.amazonaws.com/id/188020E1CD2C0191ECE6A6C32DC95755:sub": "system:serviceaccount:autoscaler:autoscaler-sa"
+                }
+            }
+        }
+    ]
+}
+POLICY
+
+  tags = module.as_label.tags
+}
